0c8eb0e57c3a0625c40f3cd06ef2abed550e96eb
r-afael <roliveira1111@gmail.com>
2022-03-02 22:16:37 -0300

    criando Derived Query para Funcionario

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import br.com.alura.spring.data.service.CrudCargoService;
 import br.com.alura.spring.data.service.CrudFuncionarioService;
 import br.com.alura.spring.data.service.CrudUnidadeTrabalhoService;
+import br.com.alura.spring.data.service.RelatoriosService;
 
 @EnableJpaRepositories
 @SpringBootApplication
@@ -23,12 +24,15 @@ public class SpringDataApplication implements CommandLineRunner {
 
 	private final CrudUnidadeTrabalhoService unidadeTrabalhoService;
 
+	private final RelatoriosService relatoriosService;
+	
 	public SpringDataApplication(CrudCargoService cargoService,
 			CrudFuncionarioService funcionarioService, 
-			CrudUnidadeTrabalhoService unidadeTrabalhoService) {
+			CrudUnidadeTrabalhoService unidadeTrabalhoService, RelatoriosService relatoriosService) {
 		this.cargoService = cargoService;
 		this.funcionarioService = funcionarioService;
 		this.unidadeTrabalhoService = unidadeTrabalhoService;
+		this.relatoriosService = relatoriosService;
 	}
 
 	public static void main(String[] args) {
@@ -42,9 +46,10 @@ public class SpringDataApplication implements CommandLineRunner {
 		while (system) {
 			System.out.println("Qual função deseja executar?");
 			System.out.println("0 - Sair");
-			System.out.println("1 - Funcionario");
-			System.out.println("2 - Cargo");
+			System.out.println("1 - Cargo");
+			System.out.println("2 - Funcionario");
 			System.out.println("3 - Unidade");
+			System.out.println("4 - Relatorios");
 			
 			Integer function = scanner.nextInt();
 
@@ -57,6 +62,9 @@ public class SpringDataApplication implements CommandLineRunner {
 					break;
 				case 3:
 					unidadeTrabalhoService.inicial(scanner);
+					break;
+				case 4:
+					relatoriosService.inicial(scanner);
 					break;
 				default:
 					System.out.println("Finalizando");
