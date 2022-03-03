a47e1ad3a3878f3ece9dc345ebe0d48c7ee42b97
r-afael <roliveira1111@gmail.com>
2022-03-02 21:27:15 -0300

    Adicionando novas entidades e relacionando-as

---
+++ src/main/java/br/com/alura/spring/data/repository/UnidadeTrabalhoRepository.java
@@ -0,0 +1,13 @@
+package br.com.alura.spring.data.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.alura.spring.data.orm.Cargo;
+import br.com.alura.spring.data.orm.Funcionario;
+import br.com.alura.spring.data.orm.UnidadeTrabalho;
+
+@Repository
+public interface UnidadeTrabalhoRepository extends CrudRepository<UnidadeTrabalho, Integer> {
+
+}
